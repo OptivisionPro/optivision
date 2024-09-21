7128e85e15bd49d404e9f3bfad3f1d9e68d86555
Opti-vision <hugo777lmao@gmail.com>
2024-09-21 02:29:31 -0300

    Amigos, responsive y mas

--- home/script.js
+++ home/script.js
@@ -9,33 +9,6 @@ function playMusic(file) {
     audio.play();
 }
 
-function showFriendInfo(name) {
-    const info = document.getElementById("info-amigo");
-    let content;
-
-    switch (name) {
-        case 'Hugo Olguín':
-            content = "Información sobre Hugo Olguín.";
-            break;
-        case 'Arian Luzcak':
-            content = "Información sobre Arian Luzcak.";
-            break;
-        case 'Agustin Carbajal':
-            content = "Información sobre Agustin Carbajal.";
-            break;
-        case 'Micaela Martinez':
-            content = "Información sobre Micaela Martinez.";
-            break;
-        case 'Martin Navarrete':
-            content = "Información sobre Martin Navarrete.";
-            break;
-        default:
-            content = "Información no disponible.";
-    }
-
-    info.innerHTML = content;
-    document.getElementById("perfil-amigo").style.display = "block";
-}
 
 function closeFriendInfo() {
     document.getElementById("perfil-amigo").style.display = "none";
@@ -59,17 +32,11 @@ function playMusic(file) {
     }
 }
 
-function abrirlistaamigos() {
-    const friendsList = document.querySelector('.lista-amigos');
-    const toggleIcon = document.querySelector('.icono-abrir');
-
-    friendsList.classList.toggle('open');
-
-    if (friendsList.classList.contains('open')) {
-        toggleIcon.textContent = '-';
-    } else {
-        toggleIcon.textContent = '+';
-    }
+function toggleListaAmigos() {
+    const listaAmigos = document.querySelector('.lista-amigos');
+    listaAmigos.classList.toggle('open');
 }
 
-
+function closeFriendInfo() {
+    document.getElementById('perfil-amigo').style.display = 'none';
+}
