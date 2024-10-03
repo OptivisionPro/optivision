46723bcc1a1dadb1b79a8117e10ccd71ceea1954
Opti-vision <hugo777lmao@gmail.com>
2024-10-03 01:12:06 -0300

    mejoras al home (barra, responsive, etc)

--- home/script.js
+++ home/script.js
@@ -40,3 +40,26 @@ function toggleListaAmigos() {
 function closeFriendInfo() {
     document.getElementById('perfil-amigo').style.display = 'none';
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.getElementById('toggle');
+    const nav = document.querySelector('.navegacion');
+    const navLinks = document.querySelectorAll('.navegacion a');
+
+    // Función para cerrar el menú
+    function closeMenu() {
+        toggle.checked = false;  // Desmarca el checkbox
+    }
+
+    // Cerrar el menú al hacer clic en cualquier enlace
+    navLinks.forEach(link => {
+        link.addEventListener('click', closeMenu);
+    });
+
+    // Cierra el menú al hacer clic fuera del menú
+    document.addEventListener('click', function(event) {
+        if (!nav.contains(event.target) && !toggle.contains(event.target)) {
+            closeMenu();
+        }
+    });
+});
