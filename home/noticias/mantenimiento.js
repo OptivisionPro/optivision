af3fdbf0f3c2881db433db3252ac134bba7e1b64
Opti-vision <hugo777lmao@gmail.com>
2024-10-17 04:23:03 -0300

    Juegos que mantenemos

---
+++ home/noticias/mantenimiento.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('.carousel-juegos').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
